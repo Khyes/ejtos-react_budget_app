b7304062ccc0189fe01fb0a693663204d347aacd
alexy piche <alexyautumn@gmail.com>
2023-10-26 13:09:21 -0400

    updated bugs

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,24 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { budget, dispatch } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+
+    const handleBudgetChange = (event) => {
+        const newBudgetValue = event.target.value;
+        setNewBudget(newBudgetValue);
+
+        // Dispatch an action to update the budget in the context
+        dispatch({ type: 'SET_BUDGET', payload: parseFloat(newBudgetValue) });
+    }
+
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: £{budget}</span>
+            <input type="number" step="10" value={newBudget} onChange={handleBudgetChange} />
+        </div>
+    );
+};
+
+export default Budget;
